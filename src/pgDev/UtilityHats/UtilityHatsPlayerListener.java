f01147e00763898cd1db98330f1383c5c44cafd0
Devil boy <kws512@hotmail.com>
2012-01-07 21:23:58 -0800

    recently generated files

---
+++ src/pgDev/UtilityHats/UtilityHatsPlayerListener.java
@@ -0,0 +1,23 @@
+package pgDev.UtilityHats;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author pgDev
+ */
+public class UtilityHatsPlayerListener extends PlayerListener {
+    private final UtilityHats plugin;
+
+    public UtilityHatsPlayerListener(UtilityHats instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code here
+}
+
